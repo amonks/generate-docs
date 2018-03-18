d4f32039812d1b16d5fcb06ddce98537d3e79d43
Andrew Monks <andrew.monks@samsara.com>
2018-03-18 03:11:03 -0700

    add glob support

--- src/index.js
+++ src/index.js
@@ -1,56 +1,47 @@
-import R from 'ramda';
-import $ from 'shelljs';
-import toc from 'markdown-toc';
-import p from 'path';
+import globby from "globby";
+import R from "ramda";
+import $ from "shelljs";
+import toc from "markdown-toc";
+import p from "path";
 
 const extractComments = str => {
-  const lines = str.split('\n');
+  const lines = str.split("\n");
 
   const commentLines = lines.reduce(
     ([lines, inComment], line) => {
-      if (inComment && line.trim() === '* @private')
-        return [lines, false];
+      if (inComment && line.trim() === "* @private") return [lines, false];
 
-      if (inComment && line.trim() === '*/')
-        return [[...lines, '', ''], false];
+      if (inComment && line.trim() === "*/") return [[...lines, "", ""], false];
 
-      if (!inComment && line.trim() === '/**')
-        return [lines, true];
+      if (!inComment && line.trim() === "/**") return [lines, true];
 
-      if (inComment)
-        return [[...lines, line.trim().slice(2)], true];
+      if (inComment) return [[...lines, line.trim().slice(2)], true];
 
       return [lines, false];
     },
     [[], false]
   )[0];
 
-  return commentLines.join('\n');
+  return commentLines.join("\n");
 };
 
 const read = relativePath => {
-  const absolutePath = p.resolve(
-    process.cwd(),
-    relativePath
-  );
+  const absolutePath = p.resolve(process.cwd(), relativePath);
   const str = $.cat(absolutePath);
   return str;
 };
 
-const generateDocs = ({ paths, output }) => {
+const generateDocs = async ({ paths: globs, output }) => {
+  const paths = await globby(globs);
   const comments = R.pipe(
     R.always(paths),
-    R.map(
-      R.ifElse(
-        R.test(/\.md$/),
-        read,
-        R.compose(extractComments, read)
-      )
-    ),
-    R.join('\n\n'),
+    R.map(R.ifElse(R.test(/\.md$/), read, R.compose(extractComments, read))),
+    R.join("\n\n"),
     toc.insert
   )();
   $.echo(comments).to(p.resolve(process.cwd(), output));
+
+  return true;
 };
 
 export default generateDocs;
